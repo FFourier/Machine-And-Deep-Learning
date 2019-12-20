b72730672d318a4fe8458d73f5af7f959b043fa0
Arnulfo Rojas <drarn96@gmail.com>
2019-12-19 23:41:11 -0500

    Explicit linear regression

---
+++ 02-LinearRegression.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_boston
+
+
+boston=load_boston()
+    #Loading the dataset
+
+x=np.array(boston.data[:,5])
+    #Input matrix
+        #We take the 5th row, that represents the average number of rooms
+
+Y=np.array(boston.target)
+    #Median value of owner-occupied homes in $1000's
+
+print(f'\nx:         {np.size(x)}\nones(506): {np.size(np.ones(506))}')
+    #Validation of sizes
+X=np.array([np.ones(506),x]).T
+    #Addition of a row full of ones for the independents factors 
+print(f'X:         {X.shape}\n\n')
+
+B=np.linalg.inv(X.T @ X) @ X.T @ Y
+    #Implementation of the derivative concerned to the mean square error
+
+print(f'X:   {X.shape}\nX.T: {X.T.shape}\nY:   {np.size(Y)}\nB:   {np.size(B)}\n')
+print(B)
+    #Dimensions summary and final result
+
+plt.scatter(x,Y,alpha=0.5)
+plt.plot([4,9],[B[0]+B[1]*4,B[0]+B[1]*9],c='red')
+plt.title('Linear Regression')
+plt.show()
